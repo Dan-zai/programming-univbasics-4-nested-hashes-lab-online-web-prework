e41021224f007977cd6a4447f92725cab7047075
Daniel Cowpertwait <daniel.cowpertwait@gmail.com>
2019-09-04 09:26:16 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -59,7 +59,8 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-  programmer_hash = {first: "I'm first!", second: "I'm second!"}
+  programmer_hash = {:yukihiro_matsumoto => {known_for => "Ruby",:languages => ["LISP", "C"] }
+  
 my_hash[:third] = "I'm third!"
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
